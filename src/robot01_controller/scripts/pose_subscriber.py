db24aad2731a03bdaddf76a968f10a7ba76ba84e
rodrigomaguinat <rodrigo.maguina.t@uni.pe>
2025-08-30 00:52:39 -0500

    modified pose_callback() on pose_subscriber.py

--- src/robot01_controller/scripts/pose_subscriber.py
+++ src/robot01_controller/scripts/pose_subscriber.py
@@ -3,7 +3,7 @@ import rospy
 from turtlesim.msg import Pose
 
 def pose_callback(msg):
-    rospy.loginfo(msg)
+    rospy.loginfo('(' + str(msg.x) + ', ' + str(msg.y) + ')')
 
 if __name__ == '__main__':
     rospy.init_node('turtle_pose_subscriber')
