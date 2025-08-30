f7e7a715b7f9e0ebd2a551a10b0368f1c4938453
rodrigomaguinat <rodrigo.maguina.t@uni.pe>
2025-08-30 00:17:49 -0500

    Subscribe pose_subscriber.py

---
+++ src/robot01_controller/scripts/pose_subscriber.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import rospy
+from turtlesim.msg import Pose
+
+def pose_callback(msg):
+    rospy.loginfo(msg)
+
+if __name__ == '__main__':
+    rospy.init_node('turtle_pose_subscriber')
+
+    sub = rospy.Subscriber('/turtle1/pose', Pose, callback=pose_callback)
+                                                # function that the subscriber going to listen to the topic
+    rospy.loginfo('Node has been started')
+    
+    rospy.spin()        # infinite loop, can exit with Ctrl+C
