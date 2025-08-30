efd76e3958addfa6928fdcfa141d7abac00298b0
rodrigomaguinat <rodrigo.maguina.t@uni.pe>
2025-08-29 20:59:17 -0500

    Rate & while first_node.py

--- src/robot01_controller/scripts/first_node.py
+++ src/robot01_controller/scripts/first_node.py
@@ -2,12 +2,11 @@
 import rospy
 
 if __name__ == '__main__':
-    rospy.init_node('test_node')    # node name (test_node)   # executable name  (first_node)
-    
-    rospy.loginfo('Hello from test node')
-    rospy.logwarn('This is a warning')
-    rospy.logerr('This is an error')
-    
-    rospy.sleep(1.0)    # 1 second
+    rospy.init_node('test_node')    # node name (test_node)   # executable name  (first_node) 
+    rospy.loginfo('Test node has been started.')
 
-    rospy.loginfo('End of program')
+    rate = rospy.Rate(10)   # 10 heads - 0.1 second (frecuency)
+
+    while not rospy.is_shutdown():      # if it receive a signal will kill the loop
+        rospy.loginfo('Hello')
+        rate.sleep()        # to control the speed of the loop
